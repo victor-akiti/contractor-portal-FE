5bde94a780ef22313917693a71f65225c8293fc3
appdevprime <142982330+appdevprime@users.noreply.github.com>
2024-11-22 16:08:26 +0100

    Added function for making request to set user as out of office

--- src/app/staff/settings/page.tsx
+++ src/app/staff/settings/page.tsx
@@ -6,12 +6,18 @@ import ButtonLoadingIcon from "@/components/buttonLoadingIcon"
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { getProtected } from "@/requests/get"
+import { postProtected } from "@/requests/post"
 
 const AccountSettings = () => {
     const [showSetOutOfOfficeModal, setShowSetOutOfOfficeModal] = useState(false)
     const [settingBeingUpdated, setSettingBeingUpdated] = useState("")
     const [availableStaff, setAvailableStaff] = useState([])
     const user = useSelector((state: any) => state.user.user)
+    const [ooData, setOOData] = useState({
+        startDate: new Date().toISOString().split('T')[0],
+        endDate: "",
+        substitute: null
+    })
 
     useEffect(() => {
         getAllAvailableStaff()
@@ -42,6 +48,54 @@ const AccountSettings = () => {
         }
     }
 
+    const updateOOData = (field, value) => {
+        let tempOOData = {...ooData}
+        
+
+        if (field === "substitute") {
+            tempOOData[field] = JSON.parse(value)
+        } else {
+            tempOOData[field] = value
+        }
+        
+
+        //if field is startDate, check if it's greater than the current end date. IF field is endDate, check if it is less than start date
+        if (field === "startDate") {
+            if (new Date(tempOOData.startDate).getTime() > new Date(tempOOData.endDate).getTime()) {
+                tempOOData.endDate = tempOOData.startDate
+                setOOData(tempOOData)
+            } else if (new Date(tempOOData.endDate).getTime() < new Date(tempOOData.startDate).getTime()) {
+                tempOOData.startDate = tempOOData.endDate
+                setOOData(tempOOData)
+            }
+
+
+        } else {
+            setOOData(tempOOData)
+        }
+    }
+
+    console.log({ooData});
+
+    const getEndDateMinimumDate = () => {
+        const endDate = new Date(new Date(ooData.startDate).getTime() + 60 * 60 * 24 * 1000).toISOString().split('T')[0]
+
+        console.log({endDate});
+
+        return endDate
+        
+    }
+    
+    const setOutOfOffice = async () => {
+        try {
+            const setOutOfOfficeRequest = await postProtected (`user/outOfOffice/set`, ooData)
+        } catch (error) {
+            console.log({error});
+            
+        }
+    }
+    
+
     
 
     return (
@@ -55,23 +109,23 @@ const AccountSettings = () => {
 
                     <div>
                         <label>Start Date</label>
-                        <input type="date" />
+                        <input min= {new Date().toISOString().split('T')[0]} type="date" onChange={(event) => updateOOData("startDate", event.target.value)} />
                     </div>
 
                     <div>
                         <label>End Date</label>
-                        <input type="date" />
+                        <input min= {getEndDateMinimumDate()} type="date" onChange={(event) => updateOOData("endDate", event.target.value)} />
                     </div>
 
                     <h4>Select Substitute</h4>
                     <p>Tasks assigned to you will be re-assigned to your substitute while you&#39;re out of office.</p>
 
-                    <select>
+                    <select onChange={(event) => updateOOData("substitute", event.target.value)}>
                         <option>Select a substitute</option>
 
                         {
                             availableStaff.map((staff: any) => {
-                                return <option key={staff._id}>{`${staff.name} - ${staff.role}`} </option>
+                                return <option value={JSON.stringify(staff)} key={staff._id}>{`${staff.name} - ${staff.role}`} </option>
                             })
                         }
                     </select>
@@ -79,7 +133,7 @@ const AccountSettings = () => {
                     <div className={styles.actionButtonsDiv}>
                         <button onClick={() => setShowSetOutOfOfficeModal(false)}>Cancel</button>
 
-                        <button>Save {settingBeingUpdated === "out of office" && <ButtonLoadingIcon />}</button>
+                        <button onClick={() => setOutOfOffice()}>Save {settingBeingUpdated === "out of office" && <ButtonLoadingIcon />}</button>
                     </div>
                 </div>
             </Modal>
