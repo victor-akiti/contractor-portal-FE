6c9724cd882d1a4f9f55e136d28ce10557c7ec03
Victor Izu-Akiti <Victor Izu-Akiti@amni1.local>
2025-09-09 10:40:06 +0100

    refactor: remove linting from stopping build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        domains: ['cloudinary.com', 'res.cloudinary.com'],
-      }
+  reactStrictMode: false,
+  images: {
+    domains: ['cloudinary.com', 'res.cloudinary.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // build never fails due to lint
+  },
 
 }
 
