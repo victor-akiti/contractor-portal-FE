ad03cf52d0e92515f0f07cac7fc86480ea09abef
Victor Izu-Akiti <Victor Izu-Akiti@amni1.local>
2025-09-09 11:38:14 +0100

    feat: switch to RTK query and reduce data load time by 99%, implement caching for data persistence, temporal access to old approvals page for testing and comparison

--- src/app/staff/approvals/rows/ParkRequestedRow.tsx
+++ src/app/staff/approvals/rows/ParkRequestedRow.tsx
@@ -7,8 +7,8 @@ export default function ParkRequestedRow({ index, companyRecord, approveParkRequ
   return (
     <tr className={[styles.parkRequestedItem, index % 2 === 0 && styles.rowDarkBackground].join(" ")}>
       <td><Link href={`/staff/vendor/${companyRecord._id}`}>{String(companyRecord.companyName).toUpperCase()}</Link><p>{companyRecord?.vendorAppAdminProfile?.email ? companyRecord?.vendorAppAdminProfile?.email : companyRecord?.contractorDetails?.email}</p></td>
+      <td><p>Stage {getL2PendingStage(companyRecord?.flags)}</p></td>
       <td><p>{companyRecord?.flags?.hold?.requestedBy?.name}</p></td>
-      <td><p>Stage {getL2PendingStage(companyRecord)}</p></td>
       <td>
         {companyRecord.endUsers && Array.isArray(companyRecord.endUsers) && companyRecord.endUsers?.length > 0 && (<></>)}
         {hasAdminPermissions(user.role) && (<>
